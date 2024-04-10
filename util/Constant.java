da1570dd6fd84c308e57c027315da0a119e327a1
Jiaqi Wang <139347307+myry07@users.noreply.github.com>
2024-04-10 09:22:37 +0200

    Add files via upload

---
+++ util/Constant.java
@@ -0,0 +1,11 @@
+package TankGame.util;
+
+@SuppressWarnings({"all"})
+public class Constant {
+    public static final int FRAM_WIDTH = 1250;
+    public static final int FRAM_HEIGHT = 750;
+    public static final String FRAM_TITLE = "TankGame";
+    public static final int FRAM_X = 100;
+    public static final int FRAM_Y = 50;
+
+}
